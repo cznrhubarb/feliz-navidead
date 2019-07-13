c814dcb5bf279611474595116644dda208eff56c
AlexRoberts <Divowashere@gmail.com>
2019-07-13 10:27:23 -0400

    hp bar changes & added to gingerdead mens

--- src/GingerBreadMan.gd
+++ src/GingerBreadMan.gd
@@ -4,11 +4,20 @@ export var Speed = 10
 
 var type = "enemy"
 var player
+var health = 100
 
 func _ready():
 	player = get_tree().get_root().find_node("Child", true, false)
+	print(health)
 
 func _physics_process(delta):
 	if player:
 		var direction = (player.position - position).normalized()
 		move_and_collide(direction * Speed * delta)
+		
+func _take_damage(value):
+	health = health - value
+	if health <= 0:
+		queue_free()
+		# TODO: VFX and kill actor
+		pass
