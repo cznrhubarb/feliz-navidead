92dac3114a1d88cc7e512b7010bdbbb272b08f0d
AlexRoberts <Divowashere@gmail.com>
2019-07-13 14:42:29 -0400

    adding gingerdeadmen hp functions!

--- UI/HP_Bar.gd
+++ UI/HP_Bar.gd
@@ -2,26 +2,29 @@ extends PanelContainer
 
 # Set Default tint to green
 var tint = Color(0,1,0) # green
+var hp_bar
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	#TODO: set "HP_TextureProgress" Progress Tint equal to "tint" var
 	pass # Replace with function body.
+	hp_bar = get_node("Panel/HP_TextureProgress")
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 #func _process(delta):
 #	pass
 
 
-func _update(health):
+func update_health(health):
 	# Update Label to reflect current / max hp
 	# set Tint of TextureProgress to reflect current HP stability level.
-	if health <75:
+	if health > 75:
+		tint = Color(0,1,0) # green
+	elif health >50:
 		tint = Color(1,1,0) # yellow
-	elif health <50:
+	elif health >25:
 		tint = Color(1,.5,0) # orange
 	elif health <25:
 		tint = Color(1,0,0) # red
-	else:
-		tint = Color(0,1,0) # green
-	#TODO: set "HP_TextureProgress" Progress Tint equal to "tint" var
+	hp_bar.tint_progress = tint
+	hp_bar.value = health	
--- src/GingerBreadMan.gd
+++ src/GingerBreadMan.gd
@@ -4,19 +4,21 @@ export var Speed = 10
 
 var type = "enemy"
 var player
+var hp_bar
 var health = 100
 
 func _ready():
 	player = get_tree().get_root().find_node("Child", true, false)
-	print(health)
+	hp_bar = get_node("HP_Bar")
 
 func _physics_process(delta):
 	if player:
 		var direction = (player.position - position).normalized()
 		move_and_collide(direction * Speed * delta)
 		
-func _take_damage(value):
+func take_damage(value):
 	health = health - value
+	hp_bar.update_health(health)
 	if health <= 0:
 		queue_free()
 		# TODO: VFX and kill actor
