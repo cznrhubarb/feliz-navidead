0fcf2774fd229dddb5c92ec6732eb07944314834
AlexRoberts <Divowashere@gmail.com>
2019-07-14 13:47:08 -0400

    update to tilemap, and spawn method

--- src/GingerFactory.gd
+++ src/GingerFactory.gd
@@ -1,16 +1,22 @@
 extends Node
 
+var ginger_scene = load("res://scn/GingerBreadMan.tscn")
+var santa_scene = load("res://scn/Santa.tscn")
+
 func _ready():
 	var scene_size = get_viewport().size
-	var ginger_scene = load("res://scn/GingerBreadMan.tscn")
-	var santa_scene = load("res://scn/Santa.tscn")
 	for i in range(10):
-		var ginger = ginger_scene.instance()
-		ginger.position = Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y))
-		#get_parent().add_child(ginger)
-		add_child(ginger)
+		spawn_ginger(Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y)))
+	spawn_santa(Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y)))
 	
+func spawn_ginger(location):
+	var ginger = ginger_scene.instance()
+	ginger.position = location
+	#get_parent().add_child(ginger)
+	add_child(ginger)
+	
+func spawn_santa(location):
 	var santa = santa_scene.instance()
-	santa.position = Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y))
+	santa.position = location
 	#get_parent().add_child(santa)
 	add_child(santa)
