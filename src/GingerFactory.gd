d3afe50381c88ec6a197fbe4d949f1746676e7b2
AlexRoberts <Divowashere@gmail.com>
2019-07-14 15:48:33 -0400

    pushing tile collision change and gingerbread spawn, and some gifts

--- src/GingerFactory.gd
+++ src/GingerFactory.gd
@@ -2,12 +2,27 @@ extends Node
 
 var ginger_scene = load("res://scn/GingerBreadMan.tscn")
 var santa_scene = load("res://scn/Santa.tscn")
+var time_start
+var time_now
+var scene_size
 
 func _ready():
-	var scene_size = get_viewport().size
+	time_start = OS.get_unix_time()
+	set_process(true)
+	scene_size = get_viewport().size
 	for i in range(10):
 		spawn_ginger(Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y)))
 	spawn_santa(Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y)))
+
+func _process(delta):
+	time_now = OS.get_unix_time()
+	var elapsed = time_now - time_start
+	var minutes = elapsed / 60
+	var seconds = elapsed % 60
+	
+	if get_child_count() < seconds / 5:
+		spawn_ginger(Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y)))
+
 	
 func spawn_ginger(location):
 	var ginger = ginger_scene.instance()
@@ -20,3 +35,4 @@ func spawn_santa(location):
 	santa.position = location
 	#get_parent().add_child(santa)
 	add_child(santa)
+	
