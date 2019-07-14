0464da1ca10ceab4b67d7b72e8a1f1867001e559
Mike <cznrhubarb@gmail.com>
2019-07-14 19:30:28 -0400

    Adding sound effect to GBM death.

--- src/Explosion.gd
+++ src/Explosion.gd
@@ -1,20 +1,11 @@
 extends Node2D
 
-# Declare member variables here. Examples:
-# var a = 2
-# var b = "text"
 var color = Color(1,1,1)
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
 	get_node("Sprite/AnimationPlayer").play("Explode")
 	modulate = color
-	
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-#func _process(delta):
-#	pass
-
+	get_node("Groan").play()
 
 func _on_Animation_complete(anim_name):
 	queue_free()
