f0ac9711295bb0c49bfb0e17dceed158b960ee00
AlexRoberts <Divowashere@gmail.com>
2019-07-14 20:14:24 -0400

    Pushing Elf Changes

--- src/Elf.gd
+++ src/Elf.gd
@@ -1,14 +1,14 @@
 extends KinematicBody2D
 
-export var Speed = 10
-export var max_follow_distance = 150
+export var Speed = 150
+export var max_follow_distance = 500
 
 # warning-ignore:unused_class_variable
 var type = "enemy"
 var player
 var hp_bar
 var health
-var max_health = 100
+var max_health = 50
 var collision_info
 var anim_player
 var factory
@@ -31,6 +31,7 @@ func _physics_process(delta):
 				var collider = collision_info.collider
 				if collider.type == "child":
 					collider.take_damage(damage)
+					die()
 		#Animation
 			anim_player.playback_speed = .5
 			if abs(direction.x) >= abs(direction.y):
@@ -50,19 +51,21 @@ func take_damage(value):
 	health -= value
 	hp_bar.update_health(health, max_health)
 	if health <= 0:
-		var game_hud = get_tree().get_root().find_node("GameHud", true, false)
-		game_hud.add_score(max_health/2)
-		# TODO: Doesn't work because the sound stops when the gbman gets deleted
-		#get_node("Groan").play()
-		queue_free()
-		factory.ginger_count -= 1
-		explode()
+		die()
 		# TODO: VFX
 		
 		# LOL SO RANDOM 
 		if str(max_health)[-1] == "0":
 			spawn_pickup(self.position)
 
+func die():
+	var game_hud = get_tree().get_root().find_node("GameHud", true, false)
+	game_hud.add_score(max_health/2)
+	# TODO: Doesn't work because the sound stops when the gbman gets deleted
+	#get_node("Groan").play()
+	queue_free()
+	factory.elf_count -= 1
+	explode()
 		
 func attack(character):
 	if player:
--- src/GingerFactory.gd
+++ src/GingerFactory.gd
@@ -2,23 +2,27 @@ extends Node
 
 var ginger_scene = load("res://scn/GingerBreadMan.tscn")
 var santa_scene = load("res://scn/Santa.tscn")
+var elf_scene = load("res://scn/Elf.tscn")
 var time_start
 var time_now
 var scene_size
 var minutes = 0
 var seconds = 0
 var ginger_count = 0
+var elf_count = 0
 var santa_count = 0
 var last_santa = 0
-
+var ysort
 
 
 func _ready():
+	ysort = get_tree().get_root().find_node("YSort", true, false)
 	time_start = OS.get_unix_time()
 	set_process(true)
 	scene_size = get_viewport().size
-	#for i in range(10):
-	#	spawn_ginger()
+	
+	for i in range(2):
+		spawn_ginger()
 
 func _process(delta):
 	time_now = OS.get_unix_time()
@@ -33,6 +37,9 @@ func _process(delta):
 
 	elif seconds > last_santa + 15 and santa_count <1:
 		spawn_santa()
+		
+	elif elf_count < seconds / 15:
+		spawn_elf()
 
 	
 func spawn_ginger():
@@ -42,15 +49,21 @@ func spawn_ginger():
 	ginger.max_health = 20 + (seconds/2) + randi() % 20
 	ginger.damage = .1 + (seconds/100)
 	ginger.Speed = 10 + (seconds/5)
-	#get_parent().add_child(ginger)
-	get_node("..").add_child(ginger)
-	#add_child(ginger)
+	add_child(ginger)
+	
+func spawn_elf():
+	elf_count += 1
+	var elf = elf_scene.instance()
+	elf.position = Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y))
+	elf.max_health = 20 + ((seconds/100))
+	elf.damage = 10
+	elf.Speed = 50 + (seconds/5)
+	add_child(elf)
+
 	
 func spawn_santa():
 	santa_count += 1
 	var santa = santa_scene.instance()
 	santa.position = Vector2(rand_range(0, scene_size.x), rand_range(0, scene_size.y))
-	#get_parent().add_child(santa)
-	#add_child(santa)
-	get_node("..").add_child(santa)
+	add_child(santa)
 	
